ab9b360763064326ded7dabceacca7733827fcb8
Gary Pettie <garypettie@gmail.com>
2020-12-18 12:28:24 +0000

    12. Targeting Enemies

--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    [SerializeField] GameObject towerPrefab;
     [SerializeField] bool isPlaceable;
 
-    void OnMouseDown() {
-        if(isPlaceable){
-            Debug.Log(transform.name);
+    void OnMouseDown()
+    {
+        if(isPlaceable)
+        {
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
         }
     }
 }
